ec631e30844ca1303c7c42eab9b1815ac375c01a
wangjiabao <840028401@qq.com>
2023-01-16 02:53:25 +0800

    ok

--- app/app/internal/biz/record.go
+++ app/app/internal/biz/record.go
@@ -243,17 +243,17 @@ func (ruc *RecordUseCase) EthUserRecordHandle(ctx context.Context, ethUserRecord
 			locationCurrentLevel = 1
 			locationCurrentMax = 2500000000000
 			currentValue = 500000000000
-			dhbAmount = 50000000000000
+			dhbAmount = 500000000000
 		} else if "100000000000000000000" == v.Amount {
 			locationCurrentLevel = 2
 			locationCurrentMax = 5000000000000
 			currentValue = 1000000000000
-			dhbAmount = 100000000000000
+			dhbAmount = 1000000000000
 		} else if "300000000000000000000" == v.Amount {
 			locationCurrentLevel = 3
 			locationCurrentMax = 15000000000000
 			currentValue = 3000000000000
-			dhbAmount = 300000000000000
+			dhbAmount = 3000000000000
 		} else {
 			continue
 		}
